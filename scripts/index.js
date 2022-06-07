f6c0ed39437e9ffd84006efba19113c5bd7bb257
Aleks Yushkov <iushk19@yandex.ru>
2022-06-07 16:36:43 +0500

    fix checkformelements

--- scripts/index.js
+++ scripts/index.js
@@ -99,8 +99,6 @@ function checkFormElements(popupObj) {
 let activePopup;
 function escEventHandler(event) {
     if (event.key === "Escape") {
-        if (activePopup !== popupImage)
-            checkFormElements(activePopup)
         closePopup(activePopup);
     };
 };
@@ -126,8 +124,6 @@ popups.forEach((popupElement) => {
         if (event.target == event.currentTarget) {
             {
                 const popupElement = this;
-                if (popupElement !== popupImage)
-                    checkFormElements(popupElement);
                 closePopup(popupElement);
             }
         }
@@ -137,8 +133,6 @@ popups.forEach((popupElement) => {
 closeButtons.forEach((closeBtn) => {
     closeBtn.addEventListener("click", function () {
         const popupElement = closeBtn.closest(".popup");
-        if (popupElement !== popupImage)
-            checkFormElements(popupElement);
         closePopup(popupElement);
     });
 });
@@ -147,6 +141,7 @@ profileEditBtn.addEventListener("click", function () {
     profileNameInput.value = profileName.textContent;
     profileNameInput.focus();
     profileTitleInput.value = profileTitle.textContent;
+    checkFormElements(popupProfile);
     openPopup(popupProfile);
 });
 
@@ -154,6 +149,7 @@ placeAddBtn.addEventListener("click", function () {
     placeNameInput.value = "";
     placeNameInput.focus();
     placeImgLinkInput.value = "";
+    checkFormElements(popupPlace);
     openPopup(popupPlace);
 });
 
@@ -161,7 +157,6 @@ const saveProfileEventHandler = (event) => {
     event.preventDefault();
     profileName.textContent = profileNameInput.value;
     profileTitle.textContent = profileTitleInput.value;
-    checkFormElements(popupProfile);
     closePopup(popupProfile);
 }
 popupProfile.addEventListener("submit", saveProfileEventHandler);
@@ -200,7 +195,6 @@ const savePlaceEventHandler = (event) => {
     event.preventDefault();
     const element = createPlace(placeNameInput.value, placeImgLinkInput.value);
     elements.prepend(element);
-    checkFormElements(popupPlace);
     closePopup(popupPlace);
 }
 
